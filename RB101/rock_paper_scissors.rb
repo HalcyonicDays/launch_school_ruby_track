5e9ee0df2b1ac9508d572aff345ced2b3b547d96
HalcyonicDays <white.zayetz@gmail.com>
2024-10-13 23:52:16 -0400

    minor stylistic edits for rubocop

--- RB101/rock_paper_scissors.rb
+++ RB101/rock_paper_scissors.rb
@@ -1,8 +1,8 @@
 class RockPaperScissors
-  def rock ;    end
+  def rock;     end
   def paper;    end
   def scissors; end
-  
+
   def tie_result
     "it's a tie!"
   end
@@ -65,8 +65,8 @@ end
 MOVES = [Rock, Paper, Scissors]
 
 def get_choice
-  puts "Select (1) Rock, (2) Paper, or (3) Scissors: "
-  response = gets.chomp.downcase[0]
+  puts "Select (1) Rock, (2) Paper, or (3) Scissors:"
+  return gets.chomp.downcase[0]  # redundant return included for clarity
 end
 
 puts "Let's play Rock-Paper-Scissors!"
@@ -80,7 +80,7 @@ loop do
             end
 
   puts MOVES.sample.new().send(choice)
-  
+
   puts "would you like to play again? y/n"
   continue = gets.chomp.downcase[0]
   break unless continue == 'y'
