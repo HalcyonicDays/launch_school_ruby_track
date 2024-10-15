916722ada9c74857ca8907b8efe5c05e102526ab
HalcyonicDays <white.zayetz@gmail.com>
2024-10-14 20:33:26 -0400

    Scorekeeping implemented

--- RB101/rock_paper_scissors.rb
+++ RB101/rock_paper_scissors.rb
@@ -1,4 +1,29 @@
 class RockPaperScissors
+  @@wins = 0
+  @@losses = 0
+
+  def self.wins
+    @@wins
+  end
+
+  def self.losses
+    @@losses
+  end
+
+  def player_wins
+    puts "player wins!"
+    @@wins += 1
+  end
+
+  def player_loses
+    puts "player loses!"
+    @@losses += 1
+  end
+
+  def self.game_over?
+    @@wins >= MATCH_SIZE || @@losses >= MATCH_SIZE
+  end
+
   def rock;     end
   def paper;    end
   def scissors; end
@@ -26,16 +51,19 @@ class Rock < RockPaperScissors
   end
 
   def paper
+    player_wins
     paper_wins
   end
 
   def scissors
+    player_loses
     rock_wins
   end
 end
 
 class Paper < RockPaperScissors
   def rock
+    player_wins
     paper_wins
   end
 
@@ -44,16 +72,19 @@ class Paper < RockPaperScissors
   end
 
   def scissors
+    player_loses
     scissors_wins
   end
 end
 
 class Scissors < RockPaperScissors
   def rock
+    player_wins
     rock_wins
   end
 
   def paper
+    player_loses
     scissors_wins
   end
 
@@ -63,6 +94,7 @@ class Scissors < RockPaperScissors
 end
 
 MOVES = [Rock, Paper, Scissors]
+MATCH_SIZE = 3
 
 def implement_advanced_rules
   require_relative 'rps_lizard_spock'
@@ -70,37 +102,52 @@ end
 
 def get_choice
   message = "Select"
-  MOVES.each_with_index { |move, idx| message << " (#{idx+1}) #{move.to_s},"}
+  MOVES.each_with_index { |move, idx| message << " (#{idx + 1}) #{move}," }
   puts message.chop << ':'
-  return gets.chomp.downcase[0]  # redundant return included for clarity
+  return gets.chomp.downcase[0] # redundant return included for clarity
 end
 
 def throw_hands
   choice = get_choice
-  hand = case 
+  hand = case
          when (1..MOVES.size).include?(choice.to_i)
-           MOVES[choice.to_i-1]
-         when MOVES.map{ |move| move.to_s.downcase[0] }.include?(choice)
-           MOVES.select{ |move| move.to_s.downcase[0].include?(choice) }.first
+           MOVES[choice.to_i - 1]
+         when MOVES.map { |move| move.to_s.downcase[0] }.include?(choice)
+           MOVES.select { |move| move.to_s.downcase[0].include?(choice) }.first
          else
-           puts "I didn't understand that; please try again" 
+           puts "I didn't understand that; please try again"
            return throw_hands
          end
   hand.to_s.downcase.to_sym
 end
+
+=begin
+def throw_hands
+  choice = get_choice
+  if (1..MOVES.size).include?(choice.to_i)
+    MOVES[choice.to_i - 1]
+  elsif MOVES.map { |move| move.to_s.downcase[0] }.include?(choice)
+    MOVES.select { |move| move.to_s.downcase[0].include?(choice) }.first
+  else
+    puts "I didn't understand that; please try again" and throw_hands
+  end
+=end
 
 puts "Let's play Rock-Paper-Scissors!"
 
 rounds = 0
 loop do
   puts MOVES.sample.new().send(throw_hands)
+  # puts MOVES.sample.new().send(throw_hands.to_s.downcase.to_sym)
   rounds += 1
+
+  break if RockPaperScissors.game_over?
 
   puts "would you like to play again? y/n"
   continue = gets.chomp.downcase
   break unless continue.start_with?('y')
 
-  if rounds == 5
+  if rounds == 2
     puts "would you like to play with advanced rules? y/n"
     if gets.chomp.downcase.start_with?('y')
       implement_advanced_rules
@@ -110,7 +157,15 @@ loop do
     end
   end
 
-  puts "Let's play again!" unless rounds == 5
+  puts "Let's play again!" unless rounds == 2
+end
+
+if RockPaperScissors.wins >= MATCH_SIZE
+  puts "You are the Grand Champion!"
+elsif RockPaperScissors.losses >= MATCH_SIZE
+  puts "You are the Grand Loser! (but we love you anyway)"
+else
+  puts "The match remains undecided!"
 end
 
 puts "Thank you for playing. Goodbye."
--- RB101/rps_lizard_spock.rb
+++ RB101/rps_lizard_spock.rb
@@ -13,44 +13,53 @@ end
 
 class Rock < RockPaperScissors
   def lizard
+    player_loses
     rock_wins
   end
 
   def alien
+    player_wins
     alien_wins
   end
 end
 
 class Paper < RockPaperScissors
   def lizard
+    player_wins
     lizard_wins
   end
 
   def alien
+    player_loses
     paper_wins
   end
 end
 
 class Scissors < RockPaperScissors
   def lizard
+    player_loses
     scissors_wins
   end
 
   def alien
+    player_wins
     alien_wins
   end
 end
 
 class Lizard < RockPaperScissors
   def rock
+    player_wins
     rock_wins
   end
 
   def paper
+    player_loses
     lizard_wins
   end
 
   def scissors
+    player_wins
     scissors_wins
   end
 
@@ -59,24 +68,29 @@ class Lizard < RockPaperScissors
   end
 
   def alien
+    player_loses
     lizard_wins
   end
 end
 
 class Alien < RockPaperScissors
   def rock
+    player_loses
     alien_wins
   end
 
   def paper
+    player_wins
     paper_wins
   end
 
   def scissors
+    player_loses
     alien_wins
   end
 
   def lizard
+    player_wins
     lizard_wins
   end
 
