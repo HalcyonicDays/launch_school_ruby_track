20ebd9369a950354cec995ba59f61967aae0ff91
HalcyonicDays <white.zayetz@gmail.com>
2024-11-25 22:30:54 -0500

    added a problem to easy_5 problem set

--- RB110/small_problems/easy_5.rb
+++ RB110/small_problems/easy_5.rb
@@ -30,3 +30,24 @@ p time_of_day(-1437) == "00:03"
 p time_of_day(3000) == "02:00"
 p time_of_day(800) == "13:20"
 p time_of_day(-4231) == "01:29"
+
+
+def after_midnight(time)
+  hours, minutes = time.split(':')
+  hours_to_add = (hours.to_i % HOURS_IN_A_DAY) * MINUTES_IN_AN_HOUR
+  hours_to_add + minutes.to_i
+end
+
+def before_midnight(time)
+  return after_midnight(time) if after_midnight(time).zero?
+  (HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR) - after_midnight(time)
+end
+
+puts "Midnight 2 tests:"
+p after_midnight('00:00') == 0
+p after_midnight('00:01') == 1
+p after_midnight('24:00') == 0
+p after_midnight('12:34') == 754
+p before_midnight('00:00') == 0
+p before_midnight('24:00') == 0
+p before_midnight('12:34') == 686
